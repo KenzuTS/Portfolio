fb023ac6066020e7330a390b46b9fe3aea68ce8b
KenzuTS <TonneauS@gmail.com>
2016-03-29 12:23:24 +0200

    portfolio v1

--- js/app.js
+++ js/app.js
@@ -2,4 +2,15 @@ $(document).ready(function() {
     $('#fullpage').fullpage({
         anchors:['home', 'about', 'work', 'contact']
     });
+
+  $('.slider').slick({ 
+  	dots : true,
+  	arrows : false,
+
+  	});
+
 });
+
+$(".btn-toggle-menu").click(function() {
+    	$(".main-menu").toggleClass("hide-menu");
+    })
